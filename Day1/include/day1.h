1e3e67998642ccd89ff0e8c5577f3d6e21bd0ccd
batunpc <batuhnpc@gmail.com>
2022-06-07 14:02:02 -0400

    adjustment in cmake

---
+++ Day1/include/day1.h
@@ -0,0 +1,9 @@
+// day1.h
+namespace DAY1 {
+class my_class {
+   public:
+    unsigned int challenge(unsigned int[], unsigned int, unsigned int);
+    unsigned int challenge2nd(unsigned int[], unsigned int, unsigned int);
+    int countLines(const char*, char**, int);
+};
+}  // namespace DAY1
--- Day1/main.cpp
+++ Day1/main.cpp
@@ -28,45 +28,20 @@ Of course, your expense report is much larger. Find the two entries that sum to 
 Your puzzle answer was 1015476. */
 
 #include <iostream>
-int countLines(const char* fname, char* line[], int max);
-unsigned int challenge(unsigned int data[], unsigned int numData, unsigned int goal);
 
-int main(int argc, char* argv[]) {
+#include "include/day1.h"
+using namespace DAY1;
+
+int main() {
+    my_class mc;
     char* lines[256] = {0};
-    int numLines = countLines("input.txt", lines, 1000);
+    int numLines = mc.countLines("input.txt", lines, 1000);
     unsigned int values[2000];
     for (int i = 0; i < numLines; i++) {
         values[i] = atoi(lines[i]);
     }
-    std::cout << challenge(values, numLines, 2020) << std::endl;
-}
-
-unsigned int challenge(unsigned int data[], unsigned int numData, unsigned int goal) {
-    int product;
-    for (unsigned int i = 0; i < numData; i++) {
-        for (unsigned int j = 0; j < numData - 1; j++) {
-            if (data[i] + data[j] == goal) {
-                product = data[j] * data[i];
-            }
-        }
-    }
-    return product;
-}
-
-int countLines(const char* fname, char* line[], int max) {
-    FILE* fp = fopen(fname, "r");
-    int rc = 0;
-    if (fp == NULL) {
-        std::cout << "unable to open " << fname << std::endl;
-        return 0;
-    } else {
-        size_t len;
-        while (rc < max && getline(&line[rc], &len, fp) != -1) {
-            int lineLength = strlen(line[rc]);
-            line[rc][lineLength - 1] = '\0';
-            rc++;
-        }
-        fclose(fp);
-    }
-    return rc;
+    std::cout << "=Challenge 1=\n"
+              << mc.challenge(values, numLines, 2020) << std::endl;
+    std::cout << "=Challenge 2=\n"
+              << mc.challenge2nd(values, numLines, 2020) << std::endl;
 }
