39f374acd906308acb420693a1890f1028fe4fa7
batu <bipci@myseneca.ca>
2022-06-09 02:54:23 -0400

    prep_2

--- Day1/src/day1.cpp
+++ Day1/src/day1.cpp
@@ -1,49 +1,51 @@
 // day1.cpp
 #include "include/day1.h"
-
+#include <cstring>
 #include <iostream>
 
 using namespace DAY1;
 
-unsigned int my_class::challenge(unsigned int data[], unsigned int numData, unsigned int goal) {
-    int product;
-    for (unsigned int i = 0; i < numData; i++) {
-        for (unsigned int j = 0; j < numData - 1; j++) {
-            if (data[i] + data[j] == goal) {
-                product = data[j] * data[i];
-            }
-        }
+unsigned int my_class::challenge(unsigned int data[], unsigned int numData,
+                                 unsigned int goal) {
+  int product;
+  for (unsigned int i = 0; i < numData; i++) {
+    for (unsigned int j = 0; j < numData - 1; j++) {
+      if (data[i] + data[j] == goal) {
+        product = data[j] * data[i];
+      }
     }
-    return product;
+  }
+  return product;
 }
-unsigned int my_class::challenge2nd(unsigned int data[], unsigned int numData, unsigned int goal) {
-    int product;
-    for (unsigned int i = 0; i < numData; i++) {
-        for (unsigned int j = 0; j < numData - 1; j++) {
-            for (unsigned int k = 0; k < numData - 1; k++) {
-                if (data[i] + data[j] + data[k] == goal) {
-                    product = data[j] * data[i] * data[k];
-                }
-            }
+unsigned int my_class::challenge2nd(unsigned int data[], unsigned int numData,
+                                    unsigned int goal) {
+  int product;
+  for (unsigned int i = 0; i < numData; i++) {
+    for (unsigned int j = 0; j < numData - 1; j++) {
+      for (unsigned int k = 0; k < numData - 1; k++) {
+        if (data[i] + data[j] + data[k] == goal) {
+          product = data[j] * data[i] * data[k];
         }
+      }
     }
-    return product;
+  }
+  return product;
 }
 
-int my_class::countLines(const char* fname, char* line[], int max) {
-    FILE* fp = fopen(fname, "r");
-    int rc = 0;
-    if (fp == NULL) {
-        std::cout << "unable to open " << fname << std::endl;
-        return 0;
-    } else {
-        size_t len;
-        while (rc < max && getline(&line[rc], &len, fp) != -1) {
-            int lineLength = strlen(line[rc]);
-            line[rc][lineLength - 1] = '\0';
-            rc++;
-        }
-        fclose(fp);
+int my_class::countLines(const char *fname, char *line[], int max) {
+  FILE *fp = fopen(fname, "r");
+  int rc = 0;
+  if (fp == NULL) {
+    std::cout << "unable to open " << fname << std::endl;
+    return 0;
+  } else {
+    size_t len;
+    while (rc < max && getline(&line[rc], &len, fp) != -1) {
+      int lineLength = strlen(line[rc]);
+      line[rc][lineLength - 1] = '\0';
+      rc++;
     }
-    return rc;
+    fclose(fp);
+  }
+  return rc;
 }
--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
-int main(int argc, char** argv){
-  std::cout << "buba";
+int read_file(const char *fname) {
+  char ch;
+  int noc = 0;
+  FILE *fp = fopen(fname, "r");
+  while (true) {
+    ch = fgetc(fp);
+    if (ch == EOF)
+      break;
+    std::cout << " " << ch << std::endl;
+    noc++;
+  }
+
+  fclose(fp);
+  return noc;
+}
+int main(int argc, char **argv) {
+  std::cout << "num of characters: " << read_file("input.txt") << std::endl;
 }
