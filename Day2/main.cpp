f22a3aa62ae43e5c252893307e6c720e1d47a02a
batunpc <batuhnpc@gmail.com>
2022-07-01 02:19:34 -0400

    🦈

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,12 +1,10 @@
 #include "include/day2.hpp"
 #include <iostream>
-#include <sstream>
-#include <stdio.h>
-#include <string.h>
 
 using namespace DAY2;
 
 int main() {
   my_class d2;
-  std::cout << d2.solve();
+  std::cout << d2.solve() << std::endl;
+  return 0;
 }
